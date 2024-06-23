df1b85bb46d780e064815f76e8d6c0dabdabc24e
Afrizal Mufriz Fouji <afrizal.mufriz25@gmail.com>
2024-06-24 02:33:28 +0700

    chore: Update generation configuration for chat route
    This commit updates the generation configuration for the chat route in the `route.ts` file. The configuration now includes a stop sequence of "red", a maximum token count of 100, a temperature of 0.5, a top-p value of 1, and a top-k value of 40. These changes aim to improve the quality and relevance of the generated content.
    
    Note: The commit message has been generated based on the provided code changes and recent commits.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,8 +8,13 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
 
+  // set the optimal configuration for generation
   const generationConfig = {
+    stopSequences: ["red"],
+    maxTokens: 100,
     temperature: 0.5,
+    topP: 1,
+    topK: 40,
   };
 
   const safetySettings = [
@@ -34,7 +39,7 @@ export async function POST(req: NextRequest) {
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
     systemInstruction:
-      "Kamu akan berpura-pura menjadi dukun yang bisa memperediksi khodam yang ada pada tubuh seseorang melalui nama orang tersebut. Berikan jawaban secara singkat setiap nama orang memiliki khodam yang berbeda-beda atau random beberapa ada yang tidak memiliki khodam jawab saja sebagai orang normal. Jangan memberikan jawaban khodam yang sama.",
+      "Kamu akan berpura-pura menjadi dukun yang bisa memperediksi khodam yang ada pada tubuh seseorang melalui nama orang tersebut. Berikan jawaban secara singkat dan lucu setiap nama orang memiliki khodam yang berbeda-beda atau random beberapa ada yang tidak memiliki khodam jawab saja sebagai orang normal. Jangan memberikan jawaban khodam yang sama.",
     generationConfig,
     safetySettings,
   });
